97c0ff5b14ea1ecad4353d10734d5c3ec9029e70
Lars Pensjo <lars.pensjo@gmail.com>
2013-06-08 22:38:53 +0200

    Update tests for new API.

--- quadtree_test.go
+++ quadtree_test.go
@@ -7,7 +7,7 @@ import (
 func TestInitial(t *testing.T) {
 	lowerLeft := [2]float64{0, 0}
 	upperRight := [2]float64{1, 1}
-	tree := MakeQuadtree(lowerLeft, upperRight, 6)
+	tree := MakeQuadtree(lowerLeft, upperRight)
 	if !tree.Empty() || tree.hasChildren || tree.numObjects > 0 || len(tree.objects) != 0 {
 		t.Error("Initial tree not empty")
 	}
