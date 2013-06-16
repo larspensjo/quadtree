b5f549f0668bacb454b73303dae6800ffd050434
Lars Pensjo <lars.pensjo@gmail.com>
2013-06-16 10:42:45 +0200

    Add basic benchmark test

--- quadtree_test.go
+++ quadtree_test.go
@@ -1,14 +1,48 @@
 package quadtree
 
 import (
+	"math/rand"
 	"testing"
 )
 
-func TestInitial(t *testing.T) {
+type o [2]float64
+
+func (obj *o) GetCurrentPosition() [2]float64 {
+	return *obj
+}
+
+func basicTree() *Quadtree {
 	lowerLeft := [2]float64{0, 0}
 	upperRight := [2]float64{1, 1}
-	tree := MakeQuadtree(lowerLeft, upperRight)
+	return MakeQuadtree(lowerLeft, upperRight)
+}
+
+func TestInitial(t *testing.T) {
+	tree := basicTree()
 	if !tree.Empty() || tree.hasChildren || tree.numObjects > 0 || len(tree.objects) != 0 {
 		t.Error("Initial tree not empty")
+	}
+
+	x1 := o{1.0, 2.0}
+	tree.Add(&x1)
+	if tree.numObjects != 1 || tree.Empty() {
+		t.Error("Expected size 1")
+	}
+
+	tree.Remove(&x1)
+	if !tree.Empty() || tree.hasChildren || tree.numObjects > 0 || len(tree.objects) != 0 {
+		t.Error("Tree should be empty")
+	}
+}
+
+func BenchmarkAdd(t *testing.B) {
+	tree := basicTree()
+	list := make([]o, t.N)
+	for _, obj := range list {
+		obj[0] = rand.Float64()
+		obj[1] = rand.Float64()
+	}
+	for _, obj := range list {
+		tree.Add(&obj)
 	}
 }
